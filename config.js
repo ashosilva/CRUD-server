8d6661bb68f7c05e4208c682dee633eb20e94325
Abayomi Shosilva <ashosilva@gmail.com>
2021-01-22 02:39:54 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGODB:
+        'mongodb+srv://ashosilva:qz7OTOWbVWXrexyA@socialmediacluster.mdcvx.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
+
